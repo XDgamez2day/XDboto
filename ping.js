207effe57b4feb2824927360cce4c41c759d4df0
XDgamez2day <64398157+XDgamez2day@users.noreply.github.com>
2020-04-27 14:23:14 +0700

    Add files via upload

---
+++ ping.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ * A ping pong bot, whenever you send "ping", it replies "pong".
+ */
+
+// Import the discord.js module
+const Discord = require('discord.js');
+const { Client, MessageAttachment } = require('discord.js');
+const client = new Client();
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+// Create an event listener for messages
+client.on('message', message => {
+  // If the message is "ping"
+  if (message.content === 'ping') {
+    // Send "pong" to the same channel
+    message.channel.send('pong');
+  }
+});
+
+client.on('message', message => {
+    // If the message is 'rip'
+    if (message.content === 'rip') {
+      // Create the attachment using MessageAttachment
+      const attachment = new MessageAttachment('https://i.imgur.com/w3duR07.png');
+      // Send the attachment in the message channel
+      message.channel.send(attachment);
+    }
+  });
+
+// Log our bot in using the token from https://discordapp.com/developers/applications/me
+client.login('NzA0MTU4MTI5OTYwNzc5ODM2.XqZJ8A.VfqvV8Vk_Gk2DjyMx9v-zEk0cR0');
